5f774f47ca06adb69c1bd376c5a4021a5b514d0f
raptor0719 <shortman_gamer@hotmail.com>
2021-08-20 23:50:01 -0400

    Add a separate ViewPanel class as we will need to implement it's own paint cycle

---
+++ src/main/raptor/modelMaker/components/ViewPanel.java
@@ -0,0 +1,14 @@
+package raptor.modelMaker.components;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class ViewPanel extends JPanel {
+	// TODO: Add plane, Add model
+
+	@Override
+	public void paintComponent(final Graphics g) {
+		// TODO: Implement draw
+	}
+}
