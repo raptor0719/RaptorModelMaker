38136de6d311618b547b9cb91f508e2c71d1df73
raptor0719 <shortman_gamer@hotmail.com>
2021-12-26 18:38:26 -0500

    Add equals and hashCode methods to Hardpoint to allow easy collection deletions

--- src/main/raptor/modelMaker/model/Hardpoint.java
+++ src/main/raptor/modelMaker/model/Hardpoint.java
@@ -36,4 +36,33 @@ public class Hardpoint {
 	private int normalizeRotation(final int rotation) {
 		return (rotation >= 0) ? rotation % 360 : 360 - ((rotation * -1) % 360);
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+
+		Hardpoint other = (Hardpoint) obj;
+
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+
+		return true;
+	}
 }
