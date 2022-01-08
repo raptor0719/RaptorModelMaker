7e9503264484fe0033feced285ebfbfdb358adaa
raptor0719 <shortman_gamer@hotmail.com>
2022-01-08 15:45:20 -0500

    Add display abbreviations

--- src/main/raptor/modelMaker/model/ViewDirection.java
+++ src/main/raptor/modelMaker/model/ViewDirection.java
@@ -6,23 +6,29 @@ import raptor.modelMaker.math.Point;
 import raptor.modelMaker.math.Vector;
 
 public enum ViewDirection {
-	NORTH(buildCameraPlane(0, 45)),
-	NORTHEAST(buildCameraPlane(45, 45)),
-	EAST(buildCameraPlane(90, 45)),
-	SOUTHEAST(buildCameraPlane(135, 45)),
-	SOUTH(buildCameraPlane(180, 45)),
-	SOUTHWEST(buildCameraPlane(225, 45)),
-	WEST(buildCameraPlane(270, 45)),
-	NORTHWEST(buildCameraPlane(315, 45));
+	NORTH(buildCameraPlane(0, 45), "N"),
+	NORTHEAST(buildCameraPlane(45, 45), "NE"),
+	EAST(buildCameraPlane(90, 45), "E"),
+	SOUTHEAST(buildCameraPlane(135, 45), "SE"),
+	SOUTH(buildCameraPlane(180, 45), "S"),
+	SOUTHWEST(buildCameraPlane(225, 45), "SW"),
+	WEST(buildCameraPlane(270, 45), "W"),
+	NORTHWEST(buildCameraPlane(315, 45), "NW");
 
 	private final Plane parameters;
+	private final String abbreviation;
 
-	private ViewDirection(final Plane parameters) {
+	private ViewDirection(final Plane parameters, final String abbreviation) {
 		this.parameters = parameters;
+		this.abbreviation = abbreviation;
 	}
 
 	public Plane getParameters() {
 		return parameters;
+	}
+
+	public String getAbbreviation() {
+		return abbreviation;
 	}
 
 	private static Plane buildCameraPlane(final int xDegrees, final int yDegrees) {
