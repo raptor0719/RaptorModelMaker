0e88d6e4ca7e33e7ef264d88599696b2d40645b6
raptor0719 <shortman_gamer@hotmail.com>
2022-01-17 13:00:46 -0500

    Support double type with Point2D

--- src/main/raptor/modelMaker/math/Point2D.java
+++ src/main/raptor/modelMaker/math/Point2D.java
@@ -4,9 +4,21 @@ public class Point2D {
 	private final int x;
 	private final int y;
 
+	private final double doubleX;
+	private final double doubleY;
+
 	public Point2D(final int x, final int y) {
 		this.x = x;
 		this.y = y;
+		this.doubleX = x;
+		this.doubleY = y;
+	}
+
+	public Point2D(final double x, final double y) {
+		this.x = (int)x;
+		this.y = (int)y;
+		this.doubleX = x;
+		this.doubleY = y;
 	}
 
 	public int getX() {
@@ -15,5 +27,13 @@ public class Point2D {
 
 	public int getY() {
 		return y;
+	}
+
+	public double getDoubleX() {
+		return doubleX;
+	}
+
+	public double getDoubleY() {
+		return doubleY;
 	}
 }
